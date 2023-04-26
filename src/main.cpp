41a4f497cb3f4077b34322aaf46e5b1ef57ffb9f
sircastico <carlosemanuelslb2@gmail.com>
2023-04-26 16:56:35 +0100

    asd

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,19 @@
 #include <vector>
 #include <chrono>
 #include <thread>
+#include <optional>
 
 #define VULKAN_HPP_NO_EXCEPTIONS
 #include <vulkan/vulkan.hpp>
 
 class Engine{
 public:
+    struct QueueFamilyIndices {
+        std::optional<uint32_t> graphicsFamily;
+        std::optional<uint32_t> computeFamily;
+        std::optional<uint32_t> transferFamily;
+    };
+
     inline static const std::string AppName    = "01_InitInstance";
     inline static const std::string EngineName = "Vulkan.hpp";
 
@@ -23,8 +30,10 @@ public:
 
     SDL_Window *_window;
     vk::Instance _instance;
+    vk::PhysicalDevice _physicalDevice; 
     vk::Device _device;
     vk::SurfaceKHR _surface;
+    vk::Queue _graphicsQueue;
 
     Engine(bool validationLayers=true) : enableValidationLayers(validationLayers){
         initSDL();
@@ -33,6 +42,7 @@ public:
 
     ~Engine(){
         _device.destroy();
+        _instance.destroySurfaceKHR(_surface);
         _instance.destroy();
         SDL_DestroyWindow(_window);
     }
@@ -82,7 +92,7 @@ private:
         err = SDL_Vulkan_LoadLibrary(nullptr);
         assert(err >= 0);
 
-        SDL_Window *_window = SDL_CreateWindow(
+        _window = SDL_CreateWindow(
             "Vulkan Engine",
             SDL_WINDOWPOS_CENTERED,
             SDL_WINDOWPOS_CENTERED,
@@ -92,6 +102,53 @@ private:
         );
         assert(_window!=nullptr);
         printf("init sdl\n");
+    }
+
+    std::optional<vk::PhysicalDevice> getSuitablePhysicalDevice(){
+        // enumerate the physicalDevices
+        auto [presult, physicalDevices] = _instance.enumeratePhysicalDevices();
+        vk::resultCheck(presult, "Error on enumerate physical devices");
+
+        std::optional<vk::PhysicalDevice> descDev = std::nullopt, intDev = std::nullopt;
+
+        for(vk::PhysicalDevice phDev : physicalDevices){
+            vk::PhysicalDeviceProperties pr = phDev.getProperties();
+            vk::PhysicalDeviceFeatures ft = phDev.getFeatures();
+            if(pr.deviceType == vk::PhysicalDeviceType::eDiscreteGpu){
+                descDev = phDev;
+                break;
+            }
+            else if(pr.deviceType == vk::PhysicalDeviceType::eIntegratedGpu){
+                intDev = phDev;
+                break;
+            }
+        }
+        if(descDev.has_value())
+            return descDev;
+        else if(intDev.has_value())
+            return intDev;
+
+        return std::nullopt;
+    } 
+
+
+    QueueFamilyIndices findQueueFamilies(vk::PhysicalDevice device) {
+        QueueFamilyIndices indices;
+        // Logic to find queue family indices to populate struct with
+        std::vector<vk::QueueFamilyProperties> qfps = device.getQueueFamilyProperties();
+        int i = 0;
+
+        for(const auto& queueFamily : qfps){
+            if(queueFamily.queueFlags & vk::QueueFlagBits::eCompute)
+                indices.computeFamily = i;
+
+            else if(queueFamily.queueFlags & vk::QueueFlagBits::eGraphics)
+                indices.graphicsFamily = i;
+
+            else if(queueFamily.queueFlags & vk::QueueFlagBits::eTransfer)
+                indices.transferFamily = i;
+        }
+        return indices;
     }
 
     void initVulkan(){
@@ -121,40 +178,54 @@ private:
         vk::resultCheck(resultValue.result, "Failed to create Vulkan Instance");
         _instance = resultValue.value;
 
-        VkSurfaceKHR srfc;
-        SDL_Vulkan_CreateSurface(_window, _instance, &srfc);
-        _surface = vk::SurfaceKHR(srfc);
+        {
+            VkSurfaceKHR surf;
+            if(!SDL_Vulkan_CreateSurface(_window, _instance, &surf)){
+
+            }
+            _surface = surf;
+        }
 
         // vk::DebugUtilsMessengerCreateInfoEXT debugMessInfo;
 
         //auto [dresult, debugUtilsMessenger] = instance.createDebugUtilsMessengerEXT( debugMessInfo );
         //vk::resultCheck(dresult, "Failed to create debugUtilsMessenger");  
 
-        // enumerate the physicalDevices
-        auto [presult, physicalDevices] = _instance.enumeratePhysicalDevices();
-        vk::resultCheck(presult, "asd");
 
-        vk::PhysicalDevice physicalDevice = physicalDevices.front();
+        auto optPhysicalDevice = getSuitablePhysicalDevice();
+        if(!optPhysicalDevice.has_value()){
+            printf("no suitable device found\n");
+            abort();
+        }
 
-        // get device with graphics
-        auto queueFamilyPrVec = physicalDevice.getQueueFamilyProperties();
-        auto propertyIterator = std::find_if( 
-            queueFamilyPrVec.begin(),
-            queueFamilyPrVec.end(),
-            []( vk::QueueFamilyProperties const & qfp ) { 
-                return qfp.queueFlags & vk::QueueFlagBits::eGraphics; 
-            } 
-        );
+        _physicalDevice = optPhysicalDevice.value();
 
-        size_t graphicsQueueFamilyIndex = std::distance( queueFamilyPrVec.begin(), propertyIterator );
-        assert( graphicsQueueFamilyIndex < queueFamilyPrVec.size() );
+        QueueFamilyIndices inds = findQueueFamilies(_physicalDevice);
+        if(!inds.graphicsFamily.has_value()){
+            printf("found no graphics queue\n");
+            abort();
+        }
 
         // Create device
-        float                     queuePriority = 0.0f;
-        vk::DeviceQueueCreateInfo deviceQueueCreateInfo( vk::DeviceQueueCreateFlags(), static_cast<uint32_t>( graphicsQueueFamilyIndex ), 1, &queuePriority );
-        auto devResultValue = physicalDevice.createDevice( vk::DeviceCreateInfo( vk::DeviceCreateFlags(), deviceQueueCreateInfo ) );
-        vk::resultCheck(devResultValue.result, "asd2");
+        float queuePriority = 1.0f;
+        vk::DeviceQueueCreateInfo deviceQueueCreateInfo(
+            vk::DeviceQueueCreateFlags(), 
+            inds.graphicsFamily.value(), 
+            1, 
+            &queuePriority 
+        );
+
+        vk::DeviceCreateInfo deviceCreateInfo = vk::DeviceCreateInfo(
+            vk::DeviceCreateFlags(), 
+            deviceQueueCreateInfo
+        );
+
+        auto devResultValue = _physicalDevice.createDevice(deviceCreateInfo);
+
+        vk::resultCheck(devResultValue.result, "failed to create device");
         _device = devResultValue.value;
+
+        _graphicsQueue = _device.getQueue(inds.graphicsFamily.value(),0);
 
     }
 
